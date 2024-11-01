067b9eb8b063b0817642fd60168f4d2a6e5f6de6
juliocesardemoraes <jcmcf2012@gmail.com>
2024-11-01 09:25:10 -0300

    infra: setup project

---
+++ main.js
@@ -0,0 +1,14 @@
+// // Import Monaco Editor's main module
+// import * as monaco from "monaco-editor";
+
+// // Initialize the editor with a dark theme and change tracking
+// const editor = monaco.editor.create(document.getElementById("container"), {
+//   value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
+//   language: "html",
+//   theme: "vs-dark",
+// });
+
+// // Track changes in the editor's content
+// // const editorContentChangeTracker = editor.onDidChangeModelContent((event) => {
+// //   console.log("Editor content changed:", editor.getValue());
+// // });
